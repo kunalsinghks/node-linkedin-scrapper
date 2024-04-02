6621fee6cf3f1800ddf16154d42a5bbecfbec7f7
Kunal Singh <77065085+kunalsinghks@users.noreply.github.com>
2024-04-02 13:23:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,107 +1,83 @@
-#add image uri
-
+import requests
+from lxml import html
+import random
+from fake_useragent import UserAgent
+import time
 import json
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from time import sleep
 
-def extract_key_value_pair(driver, xpath):
+def scrape_linkedin_company(company_id):
+    url = company_id
+
+    # Create a random User-Agent
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+
+    # Add a random delay between requests (between 1 and 5 seconds)
+    delay = random.uniform(1, 5)
+    time.sleep(delay)
+
     try:
-        element = driver.find_element(By.XPATH, xpath)
-        category = element.find_element(By.TAG_NAME, 'dt').text.strip()
-        value = element.find_element(By.TAG_NAME, 'dd').text.strip()
-        return category, value
-    except Exception as e:
-        print(f"Error extracting key-value pair: {e}")
-        return None, None
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching company data: {e}")
+        return {"error": str(e)}
 
-def scrape_company_data(url):
-    try:
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-
-        # Initialize Chrome WebDriver
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-
-        driver.get(url)
-
-        # Wait for the page to load
-        sleep(5)  
-
-        try:
-            # Find the pop-up window element
-            popup_element = driver.find_element(By.XPATH, '//*[@id="organization_guest_contextual-sign-in"]/div/section/button').click()
-            print("Pop-up window closed successfully.")
-        except Exception as e:
-            print(f"No pop-up window found or an error occurred: {e}")
-
-        # Extract company data
+    if response.status_code == 200:
+        parser = html.fromstring(response.text)
         company_data = {}
 
         # Include URL
         company_data['url'] = url
 
-        company_name = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/section/div/div[2]/div[1]/h1').text.strip()
-        about_us = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/section[1]/div/p').text.strip()
-        company_data['about'] = about_us
-        company_data['companyName'] = company_name
+        # Company Name
+        company_name = parser.xpath('//*//*[@id="main-content"]/section[1]/section/div/div[2]/div[1]/h1/text()')
+        company_data['companyName'] = company_name[0].strip() if company_name else ""
 
+        # About Us
+        about_us = parser.xpath('//*//*[@id="main-content"]/section[1]/div/section[1]/div/p/text()')
+        company_data['about'] = about_us[0].strip() if about_us else ""
+
+        # Other details
         xpaths = [
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[1]',  # Website
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[2]',  # Industry
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[3]',  # Company Size
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[4]',  # Headquarters
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[5]',  # Founded
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[6]',  # Type
-            '//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[7]'   # Specialties
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[1]',  # Website
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[2]',  # Industry
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[3]',  # Company Size
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[4]',  # Headquarters
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[5]',  # Founded
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[6]',  # Type
+            '//*//*[@id="main-content"]/section[1]/div/section[1]/div/dl/div[7]',  # Specialties
+            '/html/body/main/section[1]/div/section[1]/div/dl/div[1]/dd/a',          # Website XPath
+            '/html/body/main/section[1]/section/div/div[1]/img/@src'                  # Avatar URL XPath
         ]
 
-        # Populate available values
         for xpath in xpaths:
-            category, value = extract_key_value_pair(driver, xpath)
-            if category:
-                camel_case_key = category.replace(" ", "").lower()
-                if camel_case_key == 'companysize':
-                    camel_case_key = 'companySize'  # Convert 'companysize' to 'companySize'
-                company_data[camel_case_key] = value
+            if xpath != '/html/body/main/section[1]/div/section[1]/div/dl/div[1]/dd/a' and xpath != '/html/body/main/section[1]/section/div/div[1]/img/@src':
+                category_element = parser.xpath(f'{xpath}/dt/text()')
+                value_element = parser.xpath(f'{xpath}/dd/text()')
+                if category_element and value_element:
+                    category = category_element[0].strip()
+                    value = value_element[0].strip()
+                    camel_case_key = category.replace(" ", "").lower()
+                    if camel_case_key == 'companysize':
+                        camel_case_key = 'companySize'
+                    company_data[camel_case_key] = value
+            elif xpath == '/html/body/main/section[1]/div/section[1]/div/dl/div[1]/dd/a':
+                website_element = parser.xpath(xpath)
+                if website_element:
+                    company_data['website'] = website_element[0].get('href')
+            elif xpath == '/html/body/main/section[1]/section/div/div[1]/img/@src':
+                image_uri = parser.xpath(xpath)
+                print(image_uri)
+                company_data['avatarUrl'] = image_uri[0] if image_uri else ''
 
-        # Scrape profile image URI
-        try:
-            profile_image_element = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/section/div/div[1]/img')
-            image_uri = profile_image_element.get_attribute("src")
-            company_data['avatarUrl'] = image_uri
-        except Exception as e:
-            print(f"Error scraping profile image URI: {e}")
+        return company_data
+    else:
+        return {"error": f"Failed to fetch company data. Status code: {response.status_code}"}
 
-        # Close the WebDriver
-        driver.quit()
+# Replace 'company_id' with the actual LinkedIn company ID
+company_id = "https://www.linkedin.com/company/amdocs"
+company_details = scrape_linkedin_company(company_id)
 
-        # Reorder the dictionary keys
-        reordered_keys = [
-            "url", "website", "companyName", "about","companySize", "headquarters", "industry","type", "specialties", "avatarUrl"
-        ]
-        reordered_data = {key: company_data[key] for key in reordered_keys if key in company_data}
-
-        return reordered_data
-
-    except Exception as ex:
-        print(f"An error occurred: {ex}")
-        return None
-
-# Example usage:
-company_url = 'https://www.linkedin.com/company/openai'
-company_data = scrape_company_data(company_url)
-if company_data:
-    print(json.dumps(company_data, indent=4))
-    json_file_path = 'company_data.json'  
-    with open(json_file_path, 'w') as jsonfile:
-        json.dump(company_data, jsonfile, indent=4)
-    print('Scraping completed. Data saved to:', json_file_path)
-else:
-    print("Scraping failed.")
+# Print the output as JSON
+print(json.dumps(company_details, indent=4))
